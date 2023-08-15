324fa4ef9912fc13ebbfb16ce257f6baec8fa94d
EvanO3 <Evan.oni@hotmail.com>
2023-08-14 23:34:59 -0400

    added functionality to the location function

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 
 // Getting the  location of where the person is and return the weather of that location
  
-const para = document.querySelector("location")
+const para = document.querySelector("#location")
+const para2 = document.querySelector("#location2")
+const para3 = document.querySelector("#location3")
+const para4 = document.querySelector("#location4")
+const para5 = document.querySelector("#location5")
 // this function allows to fetch for the temp
-async function fetchLocation(){
-   const response = await fetch("https://api.weatherapi.com/v1/current.json?key=b735c520ef654522aa922732230808&q=Toronto",{mode:'cors'})
-   response.json().then(function(response){
-    console.log("It is " +response.current.condition.text)
-    console.log("Temp in Celcius : " +response.current.temp_c)
-    console.log("Temp in Farenheit: "+ response.current.temp_f)
-    console.log("Feels like : "+ response.current.feelslike_c)
-    console.log("Humidity: " + response.current.humidity)
-  
-   })
-} 
 
-fetchLocation()
+const btn = document.querySelector('#btn')
+const searchBar = document.querySelector('#searchBar')
+btn.addEventListener('click', async()=>{
+    const searchTerm = searchBar.value; 
+    if(searchTerm){
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b735c520ef654522aa922732230808&q=${encodeURIComponent(searchTerm)}`,{mode:'cors'})
+        response.json().then(function(response){
+            para.innerHTML ="It is " +response.current.condition.text;
+            para2.innerText="Temp in Celcius : " +response.current.temp_c;
+            para3.innerText= "Temp in Farenheit :" + response.current.temp_f;
+            para4.innerText= "Feels like : "+ response.current.feelslike_c;
+            para5.innerText="Humidity: " + response.current.humidity;
+            para.inn
+
+        })
+    }
+})
+ 
+   
+
+
+
+  
+
